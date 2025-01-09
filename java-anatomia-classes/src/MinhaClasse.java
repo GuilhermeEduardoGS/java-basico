ccdfb03ba3469e7c9b2bf49ac4291367f3ef2f7a
GuilhermeEduardoGS <guilherme.gsouza@sptech.school>
2025-01-09 09:28:37 -0300

    Primeiras variáveis e primeiro método

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -2,8 +2,16 @@ public class MinhaClasse {
 
     public static void main(String[] args) {
 
-        System.out.print("Minha primeira estrutura");
+        String primeiroNome = "Guilherme";
+        String segundoNome = "Souza";  
 
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
     }
 
 }
